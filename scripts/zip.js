e468eb98ec208f65e8f02885d076f651a1faea20
Julien Wilson <julienawilson@gmail.com>
2016-11-07 10:10:48 -0800

    finished!

--- scripts/zip.js
+++ scripts/zip.js
@@ -21,14 +21,51 @@
           coordinates: {
             lat: feat.geometry.coordinates[1],
             lng: feat.geometry.coordinates[0]
-          }
-          }
+              }
+            }
       })
 
-       console.log(zipProps);
+
+      var neighsOnly = []
+      var topFiveNeighs = [];
+
+       zipProps.map(function(elem,idx,arr){
+          var zipNum = 0;
+          arr.forEach(function(obj){
+            if(obj.neighborhood===elem.neighborhood){
+                 zipNum=zipNum+1;
+               };
+             });
+
+             return {
+                 neighborhood: elem.neighborhood,
+                 zipCodes: zipNum
+               };
+
+        })
+        .forEach(function(elem){
+          if(!(neighsOnly.find(function(obj){
+            return obj.neighborhood ===elem.neighborhood;
+          }))){
+            neighsOnly.push(elem);
+          }
+        });
+
+          topFiveNeighs= neighsOnly.sort(function(a,b){
+            return (b.zipCodes - a.zipCodes)
+          })
+          .filter(function(elem,idx,array){
+               return idx<5;
+             });
+
+        console.log(topFiveNeighs);
+        console.log(neighsOnly);
+
     });
   };
 
   getData();
-  module.zip = zip;
+
+
+module.zip = zip;
 })(window);
