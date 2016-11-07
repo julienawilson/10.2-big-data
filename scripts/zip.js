672ba606fd51927e36ed910f6d833780bafc16e0
Julien Wilson <julienawilson@gmail.com>
2016-11-06 17:43:36 -0800

    created an array of arrays that contain feature properties

--- scripts/zip.js
+++ scripts/zip.js
@@ -1,9 +1,21 @@
 (function(module) {
   var zip = {};
 
+  var zipProps =[];
+
   getData = function() {
     $.getJSON('/data/manhattan.json', function(data) {
       // TODO: start here!
+      zipProps=data.features.map(function(feat,idx){
+
+        var newArray=[];
+        $.each(feat.properties, function(key,val){
+          newArray.push(val);
+        });
+        return newArray;
+      });
+
+       console.log(zipProps);
     });
   };
 
